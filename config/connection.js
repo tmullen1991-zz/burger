86cd5ee370e96ca6a18857a513e9a9d78bb6632f
tmullen <tmullen1991@gmail.com>
2019-02-21 19:55:18 -0700

    fixed jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "bEUDeWI5Hz!*",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bEUDeWI5Hz!*",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
